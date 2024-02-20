a8f5af59ea8b0795a845da36233990692ab920e5
Artos <ptektov@gmail.com>
2024-02-20 16:50:43 +0300

    0.0

--- bin/testing_grounds.dart
+++ bin/testing_grounds.dart
@@ -1,14 +1,5 @@
 import 'del.dart';
 
-void main() {
-  // Del del = Del('lay');
-  // print(del.name);
-  // Del del1 = Del('aefs');
-  // print(del.name);
-  // print(del1.name);
-  List<int> a;
-  a = [1, 2, 3, 4, 5, 6];
-
-  // a = a.map((e) => e * 2).toList();
-  a.forEach((element) => print(a));
+void main()
+{
 }
